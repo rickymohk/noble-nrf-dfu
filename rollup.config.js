7c552ccc057e1ebac31248b66ce1fb443e00e597
rickymo <ricky.mo@beluntech.com>
2024-10-14 17:39:28 +0800

    update rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,5 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import builtins from 'rollup-plugin-node-builtins';
-import globals from 'rollup-plugin-node-globals';
-// import { eslint } from 'rollup-plugin-eslint';
-// import pkg from './package.json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { readFileSync } from 'fs';
 
@@ -19,12 +15,9 @@ export default {
     },
     plugins: [
         json(),
-        // eslint(),
         resolve({ preferBuiltins: true }),
-        builtins(),
         commonjs({
             include: 'node_modules/**',
         }),
-        globals(),
     ],
 };
