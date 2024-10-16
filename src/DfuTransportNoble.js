a2043abd25d3e5c011114fedebfaff97250b4a06
rickymo <ricky.mo@beluntech.com>
2024-10-16 12:40:32 +0800

    increase timeout for discovering characteristics

--- src/DfuTransportNoble.js
+++ src/DfuTransportNoble.js
@@ -165,7 +165,7 @@ export default class DfuTransportNoble extends DfuTransportPrn {
             new Promise((res, rej) => {
                 setTimeout(
                     () => rej(new DfuError(ErrorCode.ERROR_TIMEOUT_FETCHING_CHARACTERISTICS)),
-                    500
+                    5000
                 );
             }),
         ])
